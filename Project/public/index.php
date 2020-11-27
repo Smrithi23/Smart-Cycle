51f3a4c8a4a3b43de02f5cc01f649d2f8766e325
Smrithi23 <smrithi.prakash23@gmail.com>
2020-11-27 14:29:31 +0530

    Modified index.php

--- Project/public/index.php
+++ Project/public/index.php
@@ -1,14 +1,5 @@
 <?php
-//   use Auth\Login;
-   
-//    if ($_SERVER['REQUEST_URI'] === '/authentication') {
-//       $result = Login($_REQUEST['username'], $_REQUEST['password']);
-//       include_once "../Templates/Auth/login_results.php";
-//    }
-    include_once "../config.php";
-    if ($result = mysqli_query($con, "SELECT * FROM Station")) {
-        echo "Returned rows are: " . mysqli_num_rows($result);
-        // Free result set
-        mysqli_free_result($result);
-    }
+   if ($_SERVER['REQUEST_URI'] === '/') {
+      include_once "../templates/user/login.php";
+   }
 ?>
