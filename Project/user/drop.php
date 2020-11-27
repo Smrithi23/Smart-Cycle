aa034e619c7ae5196ca4173fbe8b2ab4ab6a5f3f
Smrithi23 <smrithi.prakash23@gmail.com>
2020-11-27 23:48:21 +0530

    Register completed, made form fields required

--- Project/user/drop.php
+++ Project/user/drop.php
@@ -9,9 +9,9 @@
     <h1>DROP CYCLE</h1>
     <h3>CYCLE NO:</h3>
     <form id="drop-form" class="form" method="POST" action="">
-        Station Name: <input type="text" name="station_name"><br>
-        Stand Name: <input type="text" name="stand_name"><br>
-        <input type="submit" name="submit" value="submit">
+        Station Name: <input type="text" name="station_name" required><br>
+        Stand Name: <input type="text" name="stand_name" required><br>
+        <input type="submit" name="submit" value="submit" required>
     </form>
 </body>
 
--- Project/user/register.php
+++ Project/user/register.php
@@ -5,14 +5,22 @@
 </head>
 
 <body>
-    <?php include_once $_SERVER['DOCUMENT_ROOT']."/modules/user/register.php"; ?>
+    <?php
+        $message = "";
+        $error = "";
+        include_once $_SERVER['DOCUMENT_ROOT']."/modules/user/register.php";
+    ?>
     <h1>REGISTER</h1>
+    <?php
+        echo $message;
+        echo $error;
+    ?>
     <form id="register-form" class="form" method="POST" action="">
-        Username: <input type="text" name="username"><br>
-        First Name: <input type="text" name="first_name"><br>
-        Last Name: <input type="text" name="last_name"><br>
-        Phone Number: <input type="tel" name="phone_number"><br>
-        Password: <input type="password" name="password"><br>
+        Username: <input type="text" name="username" required><br>
+        First Name: <input type="text" name="first_name" required><br>
+        Last Name: <input type="text" name="last_name" required><br>
+        Phone Number: <input type="tel" name="phone_number" required><br>
+        Password: <input type="password" name="password" required><br>
         <input type="submit" name="register-submit" value="Register">
     </form>
 </body>
